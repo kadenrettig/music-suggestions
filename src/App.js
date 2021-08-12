d1a7d19dc1863ca8513c228d6dd4267d1c8aa4e3
kadenrettig <kadenrettig@gmail.com>
2021-08-12 00:20:23 -0400

    Feat: Basic Project Set-up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from "react";
-import Example from "./components/Example";
+import Spotify from "./components/Spotify";
+
 const App = () => {
   return (
-    <div className="w-full h-screen flex flex-row justify-center">
-      <div className="flex flex-col justify-center">
-        <Example label="Tracecamp CRA Template" />
+    <>
+      <div className="pt-5 pb-5 h-32 bg-gray-900 flex grid cols-1 items-center justify-items-center">
+        <label className="text-white text-xl font-spotify" for="searchBox">
+          What Are You Looking For?
+        </label>
+        <textarea
+          className="font-mono pt-2 resize-none overflow-hidden rounded-lg h-12 w-1/3 text-center border-4 "
+          id="searchBox"
+          name="searchSpotify"
+          placeholder="Artists, songs, or podcasts"
+        ></textarea>
       </div>
-    </div>
+
+      <div className="border-t-4 border-gray-700 flex items-center justify-center h-screen bg-green-900">
+        <div className="xl:w-1/2 w-4/5 flex items-center justify-center text-white border-4 border-white">
+          <Spotify />
+        </div>
+      </div>
+    </>
   );
 };
 
---
+++ src/components/Spotify.js
@@ -0,0 +1,14 @@
+function Spotify() {
+  return (
+    <iframe
+      src="https://open.spotify.com/embed/playlist/7KUi4Flw7POAq2r9iVrMyv"
+      width="100%"
+      height="385"
+      frameBorder="0"
+      allowtransparency="true"
+      allow="encrypted-media"
+    ></iframe>
+  );
+}
+
+export default Spotify;
